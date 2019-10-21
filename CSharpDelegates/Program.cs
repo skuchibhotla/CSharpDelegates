8041ac733acbc4982621d27af249502c71e3b04e
Shashank Kuchibhotla <skuchibhotla@outlook.com>
2019-10-20 22:15:39 -0400

    Complete code

--- CSharpDelegates/Program.cs
+++ CSharpDelegates/Program.cs
@@ -10,6 +10,27 @@ namespace CSharpDelegates
     {
         static void Main(string[] args)
         {
+            MyClass obj = new MyClass();
+            obj.LongRunning(CallBack);
+        }
+
+        static void CallBack(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public class MyClass
+    {
+        // MyClass will communicate with Program class using this delegate. 
+        public delegate void CallBack(int i);
+        public void LongRunning(CallBack obj)
+        {
+            for(var i = 0; i<1000; i++)
+            {
+                // Does something
+                obj(i);
+            }
         }
     }
 }
